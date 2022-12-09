3f2d975d6ccad29cc027505b6556711aad102bf5
MOHAN REDDY <51491741+killerboy101251@users.noreply.github.com>
2022-12-09 17:41:06 +0530

    Add files via upload
    09DEC JAVA ASSIGNMENT

---
+++ UserDetails.java
@@ -0,0 +1,15 @@
+public class UserDetails {
+String s;
+int age;
+double dob;
+String new_AccountNumber;
+double balance;
+   UserDetails(String s,int age,long dob,String new_AccountNumber,double balance)
+   {
+       this.s=s;
+       this.age=age;
+       this.dob=dob;
+       this.new_AccountNumber=new_AccountNumber;
+       this.balance=balance;
+   }
+}
